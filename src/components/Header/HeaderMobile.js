4cc4d52534ff9f73254b42adefa872bd59200a84
MrPhysix <redronnie777@gmail.com>
2022-06-16 18:23:52 +0300

    remove react spring anim

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -1,49 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../images/logo.svg';
 import './HeaderMobile.css';
 import usePage from '../../hooks/usePage';
-
-function Menu({
-  menuIsOpen, handlerMenuOpen, pageMain, pageMovies, pageSavedMovies,
-}) {
-  return (
-    <div className="menu__background">
-      <nav className={`menu__nav ${menuIsOpen && 'open'}`}>
-        <ul className="menu__nav_links">
-          <Link
-            to="/"
-            onClick={handlerMenuOpen}
-            className={`menu__nav_link link-hover ${pageMain && 'active'}`}
-          >
-            Главная
-          </Link>
-          <Link
-            to="/movies"
-            onClick={handlerMenuOpen}
-            className={`menu__nav_link link-hover ${pageMovies && 'active'}`}
-          >
-            Фильмы
-          </Link>
-          <Link
-            to="/saved-movies"
-            onClick={handlerMenuOpen}
-            className={`menu__nav_link link-hover ${pageSavedMovies && 'active'}`}
-          >
-            Сохраненые фильмы
-          </Link>
-        </ul>
-        <Link
-          to="/profile"
-          onClick={handlerMenuOpen}
-          className="profile_button mobile button-hover"
-        >
-          Аккаунт
-        </Link>
-      </nav>
-    </div>
-  );
-}
 
 function HeaderMobile({ isLogged }) {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -52,12 +11,22 @@ function HeaderMobile({ isLogged }) {
     usePage('/movies'),
     usePage('/saved-movies'),
   ];
-
   const style = `header__mobile_menu ${menuIsOpen && 'open'}`;
 
+  // handlers
   const handlerMenuOpen = () => {
     setMenuIsOpen(!menuIsOpen);
   };
+
+  const handleOutsideClick = (evt) => {
+    const outside = evt.target === evt.currentTarget;
+    return outside && handlerMenuOpen();
+  };
+
+  useEffect(() => { // scroll stop
+    if (menuIsOpen) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'unset';
+  }, [menuIsOpen]);
 
   return (
     <>
@@ -75,13 +44,45 @@ function HeaderMobile({ isLogged }) {
       </div>
       {
         menuIsOpen && (
-        <Menu
-          menuIsOpen={menuIsOpen}
-          handlerMenuOpen={handlerMenuOpen}
-          pageMain={pageMain}
-          pageMovies={pageMovies}
-          pageSavedMovies={pageSavedMovies}
-        />
+        <div
+          className="menu__background"
+          aria-hidden="true"
+          role="button"
+          onClick={handleOutsideClick}
+        >
+          <nav className="menu__nav">
+            <ul className="menu__nav_links">
+              <Link
+                to="/"
+                onClick={handlerMenuOpen}
+                className={`menu__nav_link link-hover ${pageMain && 'active'}`}
+              >
+                Главная
+              </Link>
+              <Link
+                to="/movies"
+                onClick={handlerMenuOpen}
+                className={`menu__nav_link link-hover ${pageMovies && 'active'}`}
+              >
+                Фильмы
+              </Link>
+              <Link
+                to="/saved-movies"
+                onClick={handlerMenuOpen}
+                className={`menu__nav_link link-hover ${pageSavedMovies && 'active'}`}
+              >
+                Сохраненые фильмы
+              </Link>
+            </ul>
+            <Link
+              to="/profile"
+              onClick={handlerMenuOpen}
+              className="profile_button mobile button-hover"
+            >
+              Аккаунт
+            </Link>
+          </nav>
+        </div>
         )
       }
     </>
