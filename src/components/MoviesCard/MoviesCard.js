ee4dc2d07cf18b683347ff530167e13d3a4605b7
MrPhysix <redronnie777@gmail.com>
2022-06-21 19:41:43 +0300

    card time h/m

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -8,6 +8,12 @@ const apiUrl = 'https://api.nomoreparties.co';
 function MoviesCard({ item, inSavedMovies }) {
   const [isSaved, setIsSaved] = useState(false);
 
+  function getHourMinuteTime(duration) {
+    const hour = Math.floor(duration / 60);
+    const minutes = duration % 60;
+    return hour > 0 ? `${hour} ч ${minutes} мин` : `${minutes} мин`;
+  }
+
   const handlerSave = () => {
     setIsSaved(!isSaved);
   };
@@ -16,19 +22,19 @@ function MoviesCard({ item, inSavedMovies }) {
     <li className="movies-card">
       <div className="movies-card__header">
         <h3 className="movies-card__title ">
-          {`${item.nameRU} (${item.year})`}
+          {item.nameRU}
         </h3>
         <data className="movies-card__time">
-          {item.duration}
-          {' '}
-          минут
+          {item.year}
+          {', '}
+          {getHourMinuteTime(item.duration)}
         </data>
       </div>
       <ExternalLink
         className="movies-card__wrapper_image"
         link={item.trailerLink}
       >
-        <img className="movies-card__image" src={apiUrl + item.image.url} alt={item.nameRU} />
+        <img className="movies-card__image" src={apiUrl + item.image.url} alt={item.image.name} />
       </ExternalLink>
       <SaveButton isSaved={isSaved} onClick={handlerSave} cardInSaved={inSavedMovies} />
     </li>
